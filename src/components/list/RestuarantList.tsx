c22ad3e733edb30e306965ac7703fa182e93a8e1
Sathvik-Shetty-1569 <sathvikshetty189@gmail.com>
2025-07-02 23:11:48 +0530

    Restaurant Card created

--- src/components/list/RestuarantList.tsx
+++ src/components/list/RestuarantList.tsx
@@ -1,10 +1,41 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { FlatList, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { cardStyles } from '@unistyles/cardStyles';
+import { useStyles } from 'react-native-unistyles';
+import RestaurantCard from './RestaurantCard';
+import CustomText from '@components/global/CustomText';
+import RecommendedList from './RecommendedList';
+import { recommendedListData } from '@utils/dummyData';
 
 const RestuarantList = () => {
+  const {styles} = useStyles(cardStyles);
+  const renderItem = ({item}:any) => {
+    return(
+      <RestaurantCard item = {item}/>
+    )
+  }
   return (
     <View>
-      <Text>RestuarantList</Text>
+
+      <CustomText
+      style = {styles.centerText}
+      fontFamily='Okra-Bold'
+      fontSize={12}
+      >1823 restaurants delivering to you</CustomText>
+      <CustomText
+      style = {styles.centerText}
+      fontFamily='Okra-Bold'
+      fontSize={12}
+      >FEATURED</CustomText>
+      <FlatList
+      renderItem={renderItem}
+      data={recommendedListData}
+      bounces={false}
+      showsVerticalScrollIndicator={false}
+      scrollEventThrottle={16}
+      keyExtractor={item => item?.id?.toString()}
+      contentContainerStyle={styles.listContainer}
+      />
     </View>
   )
 }
